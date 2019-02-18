4507333cd4d8981b3cedb7f37d7cdf71de9b4ef0
Jordan Lanoue <rlanoue@ou.edu>
2019-02-18 11:33:35 -0600

    Save

--- Todo.java
+++ Todo.java
@@ -1,4 +1,62 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-14-2
+OU ID_______________: 113475042
+OU EMAIL____________: rlanoue@ou.edu
 
-public class Todo {
+Tasks:
+1. CalculatorTest.java - complete test 2Token validHave
+2. complete 2Token invalid 
+3. complete 3Token add
+4. complete 3Token sub
+5. complete 3Token divide
+6. complete 3Token invalid
+7. complete executeTestValid
+8. complete testValid3Tokens
+9. complete testInvalidTokenLenghth
+10. complete parseDivideby0
+11. complete parseInvalidNumber
+12. complete parseInvalidToken
+13. Calculator.java - complete calculate2Tokens
+14. complete calculate3Tokens
+15. complete execute method
+16. complete parseandExecute method
+17. 
 
-}
+Estimated Time (minutes):
+1. 30
+2. 20
+3. 30
+4. 20
+5. 20
+6. 20
+7. 15
+8. 15
+9. 20
+10. 10
+11. 10
+12. 10
+13. 30
+14. 20
+15. 40
+16. 20
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 10
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 10+
+16. 25 + 
+
+*/}
